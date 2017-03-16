970bf8b7d4ba9b9254a71705b2518700d42e8ed2
Nick K <nkerns25@gmail.com>
2017-03-16 17:19:45 -0400

    [CSE 232] Finished assignment 8

--- CSE 232/assignment8/main.cpp
+++ CSE 232/assignment8/main.cpp
@@ -2,17 +2,35 @@
 #include "GUI/Graph.h"
 #include "GUI/Simple_window.h"
 
-class Bezier : Function
+class Bezier : public Shape
 {
 public:
-    Bezier(Point p1, Point p2, Point p3, int t);
-
-    void draw_lines() const override;
-
+    Bezier(Point p1, Point p2, Point p3, int t)
+    {
+        double rate = 1.0 / (double)t;
+        for (double locT = 0.0; locT <= 1.0; locT += rate)
+        {
+            double inv = 1 - locT;
+            // (1-t)^2 * P1 + 2(1-t)*t*P2 + t^2*P3
+            double x = inv*inv * (double)p1.x + 2.0 * inv * locT * (double)p2.x + locT * locT * (double)p3.x;
+            double y = inv*inv * (double)p1.y + 2.0 * inv * locT * (double)p2.y + locT * locT * (double)p3.y;
+            add(Point((int) x, (int) y));
+        }
+    }
 };
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Simple_window window(Point(20, 20), 500, 500, "Bezier");
+    Bezier b(Point(10, 10), Point(20, 400), Point(50, 300), 100);
+    b.set_color(Color::blue);
+    Bezier b1(Point(10, 300), Point(200, 0), Point(500, 300), 100);
+    b1.set_color(Color::green);
+    Bezier b2(Point(300, 10), Point(0, 200), Point(300, 500), 100);
+    b2.set_color(Color::magenta);
+    window.attach(b);
+    window.attach(b1);
+    window.attach(b2);
+    window.show();
+    return gui_main();
 }
