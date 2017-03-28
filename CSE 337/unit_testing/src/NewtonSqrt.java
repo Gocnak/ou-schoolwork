28611c22b354245c6c724451aace02c9396698cc
Nick K <nkerns25@gmail.com>
2017-03-28 15:30:21 -0400

    [CSE 337] Started unit testing assignment

---
+++ CSE 337/unit_testing/src/NewtonSqrt.java
@@ -0,0 +1,23 @@
+/**
+ * Created by nick on 3/27/17.
+ */
+public class NewtonSqrt {
+    // Newton's algorithm to calculate the square root of a
+    // with error less than e.
+    public static double newton_sqrt(double a, double e) throws Exception
+    {
+        if (a < 0 || e <= 0.0)
+            throw new Exception("Your inputs are bad!");
+
+        double x = 0.5 * a;
+        double y = (x*x + a) / (2.0 * x);
+
+        while (Math.abs(x-y) > e)
+        {
+            x = y;
+            y = (x*x + a) / (2.0 * x);
+        }
+
+        return y;
+    }
+}
---
+++ CSE 337/unit_testing/src/NewtonSqrtTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by nick on 3/27/17.
+ */
+public class NewtonSqrtTest {
+    @org.junit.Test
+    public void newton_sqrt() throws Exception {
+
+        double[] asserted = {};
+
+
+
+    }
+
+}
