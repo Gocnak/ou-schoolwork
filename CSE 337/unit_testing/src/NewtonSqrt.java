a1e7a9e8a9fe6e47f375594f7f545f489d018367
Nick K <nkerns25@gmail.com>
2017-03-31 00:11:15 -0400

    [CSE 337] Finished unit testing assignment

--- CSE 337/unit_testing/src/NewtonSqrt.java
+++ CSE 337/unit_testing/src/NewtonSqrt.java
@@ -2,12 +2,20 @@
  * Created by nick on 3/27/17.
  */
 public class NewtonSqrt {
-    // Newton's algorithm to calculate the square root of a
+
+    public static class NewtonException extends Exception
+    {
+        public NewtonException(String message)
+        {
+            super(message);
+        }
+    }
+    // Newton's algorithm to calculate the square root of a number (a)
     // with error less than e.
     public static double newton_sqrt(double a, double e) throws Exception
     {
         if (a <= 0.0 || e <= 0.0)
-            throw new Exception("Your inputs are bad!");
+            throw new NewtonException("Bad Inputs!");
 
         double x = 0.5 * a;
         double y = (x*x + a) / (2.0 * x);
--- CSE 337/unit_testing/src/NewtonSqrtTest.java
+++ CSE 337/unit_testing/src/NewtonSqrtTest.java
@@ -1,21 +1,32 @@
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by nick on 3/27/17.
  */
 public class NewtonSqrtTest {
-    @org.junit.Test(expected = Exception.class)
+
+
+    double[] testA = {-10.0, 4.0, 64.0, -10.0, -10.0, -10.0, 0.0, 0.0, 0.0, 25.0, 25.0, 25.0, -1.0, -1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0};
+    double[] testE = {0.0, 0.05, 0.05, -10.0, 0.0, 10.0, -10.0, 0, 10.0, -10.0, 0.0, 0.01, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0};
+
+    // 0 means exception here
+    double[] expected = {0, 2.0, 8.0, 0.0, 0.0, 0.0, 0.00, 0.0, 0.0, 0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.25};
+
+    @org.junit.Test
     public void newton_sqrt() throws Exception {
-
-        double[] testA = {};
-        double[] testE = {};
-
-        double[] expected = {};
-
         for (int i = 0; i < expected.length; i++)
         {
-            assertEquals(NewtonSqrt.newton_sqrt(testA[i], testE[i]), expected[i], 0.01);
+            try {
+                assertEquals(expected[i], NewtonSqrt.newton_sqrt(testA[i], testE[i]), testE[i]);
+            }
+            catch (Exception e)
+            {
+                assertTrue(e instanceof NewtonSqrt.NewtonException);
+            }
         }
+
     }
 
 }
