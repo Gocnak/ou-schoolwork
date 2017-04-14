29248b6abfc1eb379928866fb7743692bd73f9e2
Nick K <nkerns25@gmail.com>
2017-04-14 12:35:03 -0400

    [CSE 232] Finished assignment 12 (regex)

---
+++ CSE 232/assignment12/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+int main()
+{
+    regex pat("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+    string line;
+    cout << "Enter an email or \"quit\" to quit: " << endl;
+    while (getline(cin, line))
+    {
+        if (line == "quit")
+            break;
+        smatch matches;
+        if (regex_match(line, matches, pat))
+        {
+            cout << "Nice email! Here it is: " << matches[0] << endl;
+        }
+        else
+            cout << "Not a valid email! Try again!" << endl;
+    }
+    return 0;
+}
