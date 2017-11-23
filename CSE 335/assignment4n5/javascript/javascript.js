dcf96b2b94b21a95963edd4cb1c78d51c8179880
Nick K <nkerns25@yahoo.com>
2017-11-23 01:45:11 -0500

    Update javascript solution for CSE 335

--- CSE 335/assignment4n5/javascript/javascript.js
+++ CSE 335/assignment4n5/javascript/javascript.js
@@ -1,5 +1,3 @@
-
-
 function NONRAND()
 {
     return [75, -18, 216, 21, -6, 352, 39, -68, -318, 182];
@@ -8,7 +6,6 @@ function NONRAND()
 
 function RANDOM()
 {
-
     var seed = 867530942069;
     function random() {
         var x = Math.sin(seed++) * 10000;
@@ -26,22 +23,23 @@ function RANDOM()
 
 function main() {
     
-    function print(array) {
+    function print(array, type) {
         array = array.sort((a, b) => a > b);
+        console.log("Here are the numbers for " + type);
         console.log(array);
         console.log("Smallest number: " + array[0]);
         console.log("Largest number: " + array[array.length - 1]);
         console.log("Average: " + array.reduce((total, num) => total += num) / array.length);
     }
-
+    
+    console.log("Calling NONRAND...");
     var nonrands = NONRAND();
-    print(nonrands);
+    print(nonrands, "NONRAND");
     
     console.log("");
+    console.log("Calling RANDOM...");
     var rands = RANDOM();
-    print(rands);
-    
+    print(rands, "RANDOM");
 }
 
 main();
-
